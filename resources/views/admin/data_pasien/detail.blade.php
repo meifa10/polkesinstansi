1a1279f55a2a1119fe2e71601c4ffd3019500382
Meifa Ratna <meifa10@example.com>
2026-04-21 09:13:42 +0700

    push9

--- resources/views/admin/data_pasien/detail.blade.php
+++ resources/views/admin/data_pasien/detail.blade.php
@@ -126,7 +126,7 @@
                                             </form>
                                         @else
                                             <span class="px-4 py-2 bg-amber-50 text-amber-600 rounded-xl text-[10px] font-black uppercase border border-amber-200 italic tracking-tighter">
-                                                Menunggu Midtrans
+                                                Menunggu pembayaran
                                             </span>
                                         @endif
                                     </div>
