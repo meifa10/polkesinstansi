cb9bdd0a7f9b67f55ca691a727cd30967d701d95
Meifa Ratna <meifa10@example.com>
2026-05-18 21:19:44 +0700

    Update sistem

--- resources/views/admin/data_pasien/detail.blade.php
+++ resources/views/admin/data_pasien/detail.blade.php
@@ -261,8 +261,7 @@
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                                             </svg>
-                                            DOKTER: {{ optional(optional($rm->pendaftaran)->dokter)->nama_dokter ?? optional($rm->pendaftaran)->nama_dokter ?? optional($rm->pendaftaran)->dokter ?? 'Tidak Diketahui' }}
-                                        </span>
+                                        DOKTER: {{ optional(optional($rm->pendaftaran)->dokter)->name ?? 'Tidak Diketahui' }}                                        </span>
                                     </div>
                                 </div>
                             </div>
