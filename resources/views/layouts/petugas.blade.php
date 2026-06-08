a32be7f22d4f6ac271551b5616cc04d5245b4363
Meifa Ratna <meifa10@example.com>
2026-06-08 22:17:06 +0700

    Update sistem

--- resources/views/layouts/petugas.blade.php
+++ resources/views/layouts/petugas.blade.php
@@ -40,6 +40,9 @@
             <a href="{{ route('petugas.laporan.diagnosa') }}" class="menu-link">
                 Laporan Diagnosa
             </a>
+            <a href="{{ route('petugas.master_penyakit.index') }}" class="menu-link">
+                Master Data Penyakit
+            </a>
         </nav>
 
         <form method="POST" action="{{ route('instansi.logout') }}" class="p-4 border-t border-emerald-600">
