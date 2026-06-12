e860b690ef9ac9055bc753171b2c45377e667009
Meifa Ratna <meifa10@example.com>
2026-06-12 10:16:41 +0700

    Update sistem

--- app/Http/Controllers/Petugas/LaporanController.php
+++ app/Http/Controllers/Petugas/LaporanController.php
@@ -9,33 +9,33 @@ use Carbon\Carbon;
 
 class LaporanController extends Controller
 {
-public function diagnosa(Request $request)
-{
-    $query = PendaftaranPoli::with('rekamMedis')
-        ->where('status', 'selesai');
+    public function diagnosa(Request $request)
+    {
+        $query = PendaftaranPoli::with('rekamMedis')
+            ->where('status', 'selesai');
 
-    // Filter tanggal hanya jika diisi
-    if ($request->filled('tanggal')) {
-        $query->whereDate('created_at', $request->tanggal);
+        // Filter tanggal hanya jika diisi
+        if ($request->filled('tanggal')) {
+            $query->whereDate('created_at', $request->tanggal);
+        }
+
+        // Filter poli hanya jika dipilih
+        if ($request->filled('poli')) {
+            $query->where('poli', $request->poli);
+        }
+
+        $laporanSemua = (clone $query)
+            ->latest()
+            ->get();
+
+        $laporan = $query
+            ->latest()
+            ->paginate(10)
+            ->withQueryString();
+
+        return view('petugas.laporan-diagnosa', compact(
+            'laporan',
+            'laporanSemua'
+        ));
     }
-
-    // Filter poli hanya jika dipilih
-    if ($request->filled('poli')) {
-        $query->where('poli', $request->poli);
-    }
-
-    $laporanSemua = (clone $query)
-        ->latest()
-        ->get();
-
-    $laporan = $query
-        ->latest()
-        ->paginate(10)
-        ->withQueryString();
-
-    return view('petugas.laporan-diagnosa', compact(
-        'laporan',
-        'laporanSemua'
-    ));
-}
 }
